323fd343aff5dc251c50cef96ec08f1ef9cb9e90
JosephXZ1 <greninjamaster04@hotmail.com>
2026-05-17 02:58:41 -0500

    pequeñas modificaciones

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,14 @@
 <template>
   <h1>You did it!</h1>
   <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    Visit <a href="https://github.com/JosephXZ1/Proyecto_Uma-Vue/tree/main" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
 </template>
 
-<style scoped></style>
+<style scoped>
+  h1, p
+  {
+    font-family: Arial, Helvetica, sans-serif;
+  }
+</style>
